eb982ec17511d84573c77351cd20aa0e20a54949
Niloufar Sabbaghian <niloufar@Niloufars-MacBook-Pro.local>
2023-01-19 14:44:16 -0800

    header & footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div>    
+      <h4>Copyright Niloufar S. 2023 </h4>
+    </div>
+  );
+}
+  
+export default Footer;
+  
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div>    
+      <h1>Niloufar S.</h1>
+      <h2>Full Stack Web Developer</h2>
+    </div>
+  );
+}
+  
+export default Header;
+  
